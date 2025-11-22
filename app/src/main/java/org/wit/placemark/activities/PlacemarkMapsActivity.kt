6e2cb1cd39ede0eb6991f8fae82409f7bfafdd7d
JHolland <20102340@mail.wit.ie>
2025-11-22 21:45:25 +0000

    PlacemarkMapsActivity

--- app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -1,23 +1,14 @@
 package org.wit.placemark.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import org.wit.placemark.R
 import org.wit.placemark.databinding.ActivityPlacemarkMapsBinding
 
 class PlacemarkMapsActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityPlacemarkMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
         binding = ActivityPlacemarkMapsBinding.inflate(layoutInflater)
@@ -25,17 +16,5 @@ class PlacemarkMapsActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_placemark_maps)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
-        }    }
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_placemark_maps)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }}
+    }
+}
