3d6495b815cb3f1b155249ae799f0e873b81eb73
JHolland <20102340@mail.wit.ie>
2025-11-22 17:17:31 +0000

    Initial commit

---
+++ Desktop/mobileappca2/mobileappca2/app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -0,0 +1,110 @@
+package org.wit.placemark.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
+import org.wit.placemark.R
+import org.wit.placemark.databinding.ActivityPlacemarkBinding
+import org.wit.placemark.main.MainApp
+import org.wit.placemark.models.PlacemarkModel
+import org.wit.placemark.helpers.showImagePicker
+import timber.log.Timber.i
+
+class PlacemarkActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPlacemarkBinding
+    var placemark = PlacemarkModel()
+    lateinit var app: MainApp
+    private lateinit var imageIntentLauncher : ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var edit = false
+
+        binding = ActivityPlacemarkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.toolbarAdd.title = title
+        setSupportActionBar(binding.toolbarAdd)
+
+        app = application as MainApp
+
+        i("Placemark Activity started...")
+
+        if (intent.hasExtra("placemark_edit")) {
+            edit = true
+            // Requires API 33+
+            // placemark = intent.getParcelableExtra("placemark_edit",PlacemarkModel::class.java)!!
+            placemark = intent.extras?.getParcelable("placemark_edit")!!
+            binding.placemarkTitle.setText(placemark.title)
+            binding.description.setText(placemark.description)
+            binding.btnAdd.setText(R.string.save_placemark)
+            Picasso.get()
+                .load(placemark.image)
+                .into(binding.placemarkImage)
+        }
+
+        binding.btnAdd.setOnClickListener() {
+            placemark.title = binding.placemarkTitle.text.toString()
+            placemark.description = binding.description.text.toString()
+            if (placemark.title.isEmpty()) {
+                Snackbar.make(it,R.string.enter_placemark_title, Snackbar.LENGTH_LONG)
+                        .show()
+            } else {
+                if (edit) {
+                    app.placemarks.update(placemark.copy())
+                } else {
+                    app.placemarks.create(placemark.copy())
+                }
+            }
+            i("add Button Pressed: $placemark")
+            setResult(RESULT_OK)
+            finish()
+        }
+
+        binding.chooseImage.setOnClickListener {
+            showImagePicker(imageIntentLauncher)
+        }
+
+        registerImagePickerCallback()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_placemark, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun registerImagePickerCallback() {
+        imageIntentLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result ->
+                when(result.resultCode){
+                    RESULT_OK -> {
+                        if (result.data != null) {
+                            i("Got Result ${result.data!!.data}")
+                            placemark.image = result.data!!.data!!
+                            Picasso.get()
+                                   .load(placemark.image)
+                                   .into(binding.placemarkImage)
+                        } // end of if
+                    }
+                    RESULT_CANCELED -> { } else -> { }
+                }
+            }
+    }
+}
