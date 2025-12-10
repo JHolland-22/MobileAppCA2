ef064892e08fee8d4c20b7a7104792cacb3e07aa
JHolland <20102340@mail.wit.ie>
2025-12-10 02:31:13 +0000

    starting to add to tickets

--- app/src/main/java/org/wit/placemark/models/TicketModel.kt
+++ app/src/main/java/org/wit/placemark/models/TicketModel.kt
@@ -9,4 +9,10 @@ import kotlinx.android.parcel.Parcelize
 data class TicketModel(var id: Long = 0,
                        var title: String? = "",
                        var description: String? = "",
+                       var ticketStatus: String = "",
+                       var stage: String = "",
+                       var teamA: String = "",
+                       var teamB: String = "",
+                       var matchDate: Long = 0L,
+                       var pitchName: String = "",
                        var image: Uri = Uri.EMPTY) : Parcelable
