08f9123848c009a22497a8367343036c6c21a4a1
JHolland <20102340@mail.wit.ie>
2025-12-16 21:52:08 +0000

    added in night mode

--- app/src/main/java/org/wit/placemark/views/dashboard/DashboardActivity.kt
+++ app/src/main/java/org/wit/placemark/views/dashboard/DashboardActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 import org.wit.placemark.R
 import org.wit.placemark.views.placemarklist.PlacemarkListView
 import org.wit.ticket.views.ticketlist.TicketListActivity
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var switch: SwitchMaterial
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +29,47 @@ class DashboardActivity : AppCompatActivity() {
         ticketsButton.setOnClickListener {
             val intent = Intent(this, TicketListActivity::class.java)
             startActivity(intent)
+        }
+
+        switch = findViewById(R.id.darkModeSwitch)
+
+        // Saving state of our app using SharedPreferences
+        val sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false)
+
+        // check for preferences
+        if (isDarkModeOn) {
+
+            // set to dark mode
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            switch.isChecked = true
+        } else {
+            // set to light mode
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            switch.isChecked = false
+        }
+
+        switch.setOnClickListener {
+
+            // check for preferences
+            if (isDarkModeOn) {
+
+                // set to light mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+                // set value of isDarkModeOn to false
+                editor.putBoolean("isDarkModeOn", false)
+                editor.apply()
+            } else {
+
+                // set to dark mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+                // set value of isDarkModeOn to true
+                editor.putBoolean("isDarkModeOn", true)
+                editor.apply()
+            }
         }
     }
 }
